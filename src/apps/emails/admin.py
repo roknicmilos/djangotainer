7ff90e51f2925a242d80657096658a3fc10a5492
Milos Roknic <roknic.milos.994@gmail.com>
2023-09-30 21:33:19 +0200

    Implement recipient_user admin display in EmailThreadAdmin

--- src/apps/emails/admin.py
+++ src/apps/emails/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 from apps.common.admin import ModelAdmin
+from apps.common.utils import get_model_admin_change_details_url
 from apps.emails.models import EmailThread
 from apps.emails.utils import render_colored_email_status_html
+from apps.users.models import User
 
 
 @admin.register(EmailThread)
@@ -12,6 +15,7 @@ class EmailThreadAdmin(ModelAdmin):
         "__str__",
         "subject",
         "recipient",
+        "recipient_user",
         "colored_status",
         "category",
     )
@@ -29,6 +33,7 @@ class EmailThreadAdmin(ModelAdmin):
                 "id",
                 "subject",
                 "recipient",
+                "recipient_user",
                 "colored_status",
             )
         }),
@@ -46,6 +51,13 @@ class EmailThreadAdmin(ModelAdmin):
     def has_change_permission(self, request, obj: EmailThread = None) -> bool:
         return False
 
+    @admin.display(description=_("recipient user"))
+    def recipient_user(self, obj: EmailThread = None) -> str:
+        if obj and (user := User.objects.filter(email=obj.recipient).first()):
+            href = get_model_admin_change_details_url(obj=user)
+            return mark_safe(f'<a href="{href}">{user.email}</a>')
+        return "-"
+
     @admin.display(description=_("status"))
     def colored_status(self, obj: EmailThread = None) -> str:
-        return "-" if not obj else render_colored_email_status_html(email_thread=obj)
+        return render_colored_email_status_html(email_thread=obj) if obj else "-"
--- src/apps/emails/tests/test_email_thread_admin.py
+++ src/apps/emails/tests/test_email_thread_admin.py
@@ -1,11 +1,14 @@
 from unittest.mock import patch
+from faker import Faker
 
 from django.contrib.admin.sites import site as admin_site
 
 from apps.common.tests import TestCase
+from apps.common.utils import get_model_admin_change_details_url
 from apps.emails.admin import EmailThreadAdmin
 from apps.emails.tests.factories import EmailThreadFactory
 from apps.users.models import User
+from apps.users.tests.factories import UserFactory
 
 
 class TestEmailThreadAdmin(TestCase):
@@ -32,3 +35,25 @@ class TestEmailThreadAdmin(TestCase):
         colored_status = self.email_thread_admin.colored_status(obj=email_thread)
         self.assertEqual(colored_status, mock_render_colored_email_status_html.return_value)
         mock_render_colored_email_status_html.assert_called_once_with(email_thread=email_thread)
+
+    def test_should_return_single_dash_when_user_recipient_does_not_exist(self):
+        email = Faker().email()
+        self.assertFalse(User.objects.filter(email=email).exists())
+
+        # When EmailThread instance is not passed:
+        recipient_user = self.email_thread_admin.recipient_user()
+        self.assertEqual(recipient_user, "-")
+
+        # When EmailThread instance is passed:
+        email_thread = EmailThreadFactory(recipient=email)
+        recipient_user = self.email_thread_admin.recipient_user(obj=email_thread)
+        self.assertEqual(recipient_user, "-")
+
+    def test_should_return_single_link_to_user_recipient_when_they_exist(self):
+        email = Faker().email()
+        user = UserFactory(email=email)
+        email_thread = EmailThreadFactory(recipient=email)
+        recipient_user = self.email_thread_admin.recipient_user(obj=email_thread)
+        href = get_model_admin_change_details_url(obj=user)
+        expected_recipient_user = f'<a href="{href}">{email}</a>'
+        self.assertEqual(recipient_user, expected_recipient_user)
